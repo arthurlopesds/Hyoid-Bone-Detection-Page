bc9716ec25f7c2a9ccbbadfe3e67a4c592cc6166
Arthur Lopes <arthurlds2@gmail.com>
2020-03-02 03:10:38 -0300

    Add botão input e Resposta com makeStyle

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,15 +3,18 @@ import Carde from './cards';
 import Slide from "@material-ui/core/Slide";
 import {Button,ButtonToolbar} from 'react-bootstrap'
 import { makeStyles } from '@material-ui/core/styles';
-import Background from '../../images/2.png';
 import Grid from '@material-ui/core/Grid';
+import primary_Background from '../../images/2.png';
+import second_Backgroud from '../../images/3.png';
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+
 
 import './styles.css';
 
 const useStyles = makeStyles({
     main:{
         height:863,
-        backgroundImage: 'url(' + Background +')',
+        backgroundImage: 'url(' + primary_Background +')',
         backgroundSize:2000,
         backgroundColor:'#E47752',
         display: 'flex',
@@ -19,6 +22,7 @@ const useStyles = makeStyles({
         alignItems:'center',
         flexDirection:'row-reverse',
     },
+
     mainCard:{
         width: '70%',
         backgroundColor:'rgba(239,140,89,0)',
@@ -26,7 +30,59 @@ const useStyles = makeStyles({
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-    }
+    },
+
+    textSlide:{
+        width: '100%',
+        height: '100%',
+        backgroundImage: 'url('+ second_Backgroud +')',
+        backgroundSize: '500px',
+        backgroundColor: '#FFF',
+    },
+    input: {
+        display: "none"
+      },
+
+    div_main_up:{
+        // backgroundColor:'#f23',
+        height:'200px',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'flex-end',
+        marginRight:'2px'
+    },
+
+    div_up: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        width: "380px",
+        height: "50px",
+        backgroundColor: "#F3F3F3",
+        cursor: "pointer"
+      },
+
+    div_arrow: {
+        borderLeft:"4px solid" /* Adiciona borda esquerda na div como ser fosse uma linha.*/,
+        height: "100%",
+        width: "1%",
+        borderColor: "#1CA78B"
+        // backgroundColor:'red'
+      },
+
+    text_up: {
+        width: "100%",
+        marginLeft:'6px',
+        fontSize:'14px'
+    
+        // backgroundColor:'orange'
+      },
+
+    arrow: {
+        // backgroundColor:'orange',
+        width: "10%"
+      }
+
 })
 
 export default function Body() {
@@ -49,7 +105,7 @@ export default function Body() {
 
         <div id="mainSlide">
             <Slide direction="right" in={checked} mountOnEnter unmountOnExit>
-                <div id="textSlide">
+                <div className={classes.textSlide}>
                     <header>
                         <div id="txt_header">
                             <h1>Detecção em imagens utilizando</h1>
@@ -57,20 +113,35 @@ export default function Body() {
                         </div>
                     </header>
                     
-                    <div id="upload">
-                        AQUI FICA O UPLOAD
+                    <div className={classes.div_main_up}>
+                        <input
+                            accept="image/* , video/wmv"
+                            className={classes.input}
+                            id="contained-button-file"
+                            multiple
+                            type="file"
+                        />
+                        <label htmlFor="contained-button-file">
+                            <div className={classes.div_up}>
+                                <div className={classes.div_arrow} />
+                                <span className={classes.text_up}>
+                                    Selecione sua imagem ou seu vídeo
+                                </span>
+                                <ArrowUpwardIcon className={classes.arrow} />
+                            </div>
+                        </label>
                     </div>
 
                     <div id="btn_obj">
                         <div id="title_bnt_obj">Objetos a detectar</div>
                         <div id="botao">
-                            <div id="btn_nvdetect">
+                            <div >
                                 <ButtonToolbar>
-                                    <Button>Hióide</Button>
+                                    <Button>Osso Hióide</Button>
                                 </ButtonToolbar>
                             </div>
                             
-                            <div id="btn_nvdetect">
+                            <div>
                                 <ButtonToolbar>
                                     <Button>Casas</Button>
                                 </ButtonToolbar>
--- src/components/Resposta/index.js
+++ src/components/Resposta/index.js
@@ -1,38 +1,141 @@
 import React from 'react';
-import {Button,ButtonToolbar} from 'react-bootstrap'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 
+import primary_Background from '../../images/2.png';
 import './styles.css';
 
-export default class Resposta extends React.Component{
-    render(){
-        return(
-            <div id="main-resp">
+const useStyles = makeStyles({
+    mainResp:{
+        height: '100%',
+        backgroundImage: 'url('+ primary_Background +')',
+        backgroundSize: '2000px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    result:{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: '60%',
+        height: '900px',
+        // backgroundColor: '#F3f'
+    },
+    desc:{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: '40%',
+        height: '900px',
+        // backgroundColor:'#fF24'
+    },
+    resultInterno:{
+        width: '80%',
+        height: '70%',
+        backgroundColor: '#F16521',
+        borderRadius: '10px 10px',
+    },
+    descInterno:{
+        width: '70%',
+        height: '70%',
+        backgroundColor: '#F1F1F1',
+        borderRadius: '10px 10px',
+        '@media (max-width:600px)': {
+            width:'100%'
+        }
+    },
+    btn_download:{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '80%',
+        // backgroundColor: 'red'
+    },
+    btn_nvdetect:{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '70%',
+        // backgroundColor: 'royalblue',
+        '@media (max-width:600px)': {
+            width:'90%'
+        },
+    },
+    title_result:{
+        width: '80%',
+        height: '8%',
+        alignItems: 'flex-end',
+        display: 'flex',
+        /* background-color: #F1F1F1; */
+        fontSize: '30px',
+        fontFamily: 'Arial, Helvetica, sans-serif',
+        color: '#F16521',
+        fontWeight: 'bold',
+        marginTop: '10px',
+        '@media (max-width:600px)': {
+            fontSize:'24px',
+            
+        }
+        
+    },
+    title_desc:{
+        width: '50%',
+        height: '8%',
+        alignItems: 'flex-end',
+        display: 'flex',
+        /* background-color: #F122; */
+        fontSize: '30px',
+        fontFamily: 'Arial, Helvetica, sans-serif',
+        color: '#F16521',
+        fontWeight: 'bold',
+        marginTop: '10px',
+        '@media (max-width:600px)': {
+            width:'85%',
+            fontSize:'23px',
+            justifyContent:'flex-start'
+        }
+    },
+    btn:{
+        color:'#F16521',
+        border: '1px solid #F16521',
 
-                <div id="result">
-                    <span id="title_result">Resultado</span>
-                    <div id="result-interno">
-                        
-                    </div>
-                    <div id="btn_download">
-                        <ButtonToolbar>
-                            <Button>Download</Button>
-                        </ButtonToolbar>
-                    </div>
-                </div>
-
-                <div id="desc">
-                    <span id="title_desc">Descrição</span>
-                    <div id="desc-interno">
-
-                    </div>
-                    
-                    <div id="btn_nvdetect">
-                        <ButtonToolbar>
-                            <Button>Nova Detecção</Button>
-                        </ButtonToolbar>
-                    </div>
-                </div>
-            </div>
-        );
     }
+
+})
+
+
+function Resposta(){
+    const classes = useStyles();
+    return(
+        <Grid className={classes.mainResp} xs={12}>
+
+            <Grid item className={classes.result} xs={12}>
+                <span className={classes.title_result}>Resultado</span>
+                
+                <Grid item className={classes.resultInterno}>
+                    
+                </Grid>
+                
+                <Grid item className={classes.btn_download}>
+                    <Button variant="outlined" className={classes.btn}>Download</Button>
+                </Grid>
+            </Grid>
+
+            <Grid item className={classes.desc}>
+                <span className={classes.title_desc}>Descrição</span>
+                
+                <Grid item className={classes.descInterno}>
+
+                </Grid>
+                
+                <Grid item className={classes.btn_nvdetect}>
+                    <Button variant="outlined" className={classes.btn}>Nova Detecção</Button>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
 }
+
+export default Resposta;
